4a936bb1c5b4c92cd4ec7b614e98fa0f1462d0c8
Preetibana <62125212+Preetibana@users.noreply.github.com>
2020-07-29 12:18:09 +1000

    Add files via upload

---
+++ server.js
@@ -0,0 +1,46 @@
+//Task 1:Run node as a web server for hosting static files
+
+var express= require('express');
+
+var app = express();
+
+let accounts= [
+{a:1, name:'George',deposit:5},
+{a:2, name:'George',deposit:15},
+{a:1, name:'George',deposit:50}
+
+]
+ app.get(function(req,res)
+ {
+
+    res.send('accounts');
+    res.write('accounts');
+ }
+ )
+ app.listen(4000);
+
+ //Port IP address
+var http = require('http')
+http.createServer(function(req,res)
+{
+res.write("Welcome Back Alien!!")
+res.end()
+
+}
+
+).listen(8080);
+
+
+
+ 
+
+// Task 2:Sum Of Two Numbers
+var a=7
+var b=8
+var c =a+b
+console.log("Sum Of Two Numbers is=" +c)
+
+
+
+
+
